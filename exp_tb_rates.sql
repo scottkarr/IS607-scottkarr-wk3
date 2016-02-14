058c1652ce7665c28d5fe51a1de3e9bfc96187a7
scottkarr <scottkarr@Scotts-MacBook-Air.local>
2016-02-14 00:29:22 -0500

    1st iteration IS607-wk3
    Tuberculosis ETL exercise
    initial creation 02.13.2016

---
+++ exp_tb_rates.sql
@@ -0,0 +1,26 @@
+﻿/* if null data exists in any count, force 0 and filter in RStudio */
+DROP TABLE IF EXISTS tb_rates;
+
+CREATE TABLE tb_rates AS
+	SELECT 	tb.country, 
+		tb.year, 
+		coalesce(tb.child+tb.adult+tb.elderly,null,0) AS cases, 
+		pop.population,
+		round(cast(coalesce(tb.child+tb.adult+tb.elderly,null,0) AS numeric)/cast(pop.population AS numeric), 6) * 100000 AS rate_per_100thsd
+	FROM 	tb, population pop
+	WHERE 	1=1
+	AND	tb.country = pop.country
+	AND	tb.year = pop.year
+;
+
+COPY (
+	SELECT 	tb.country, 
+		tb.year, 
+		coalesce(tb.child+tb.adult+tb.elderly,null,0) AS cases, 
+		pop.population,
+		round(cast(coalesce(tb.child+tb.adult+tb.elderly,null,0) AS numeric)/cast(pop.population AS numeric), 6) * 100000 AS rate_per_100thsd
+	FROM 	tb, population pop
+	WHERE 	1=1
+	AND	tb.country = pop.country
+	AND	tb.year = pop.year
+) TO '/tmp/tbrates.csv' With CSV Header;
---
+++ imp_population.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE population
+(
+	COUNTRY varchar, 
+	YEAR int, 
+	POPULATION int
+);
+COPY population FROM '/private/tmp/population.csv' DELIMITER ',' CSV HEADER;
